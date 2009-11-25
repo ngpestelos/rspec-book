74a801d03304ac2b628680b2fd5d2fb17b09c57c
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-11-25 08:56:32 +0800

    refactor

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,26 +1,34 @@
+def messenger
+  @messenger ||= StringIO.new
+end
+
+def game
+  @game ||= Codebreaker::Game.new(messenger)
+end
+
+def messages_should_include(message)
+  messenger.string.split("\n").should include(message)
+end
+
 Given /^I am not yet playing$/ do
 end
 
+Given /^the secret code is (. . . .)$/ do |code|
+  game.start(code.split)
+end
+
+When /^I guess (. . . .)$/ do |code|
+  game.guess(code.split)
+end
+
 When /^I start a new game$/ do
-  @messenger = StringIO.new
-  game = Codebreaker::Game.new(@messenger)
   game.start(%w[r g y c])
 end
 
 Then /^I should see "([^\"]*)"$/ do |message|
-  @messenger.string.split("\n").should include(message)
-end
-
-Given /^the secret code is (. . . .)$/ do |code|
-  @messenger = StringIO.new
-  @game = Codebreaker::Game.new(@messenger)
-  @game.start(code.split)
-end
-
-When /^I guess (. . . .)$/ do |code|
-  @game.guess(code.split)
+  messages_should_include(message)
 end
 
 Then /^the mark should be (.*)$/ do |mark|
-  @messenger.string.split("\n").should include(mark)
+  messages_should_include(mark)
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,56 +2,44 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 module Codebreaker
   describe Game do
-    context "starting up" do
-      before(:each) do
-        @messenger = mock("messenger").as_null_object
-        @game = Game.new(@messenger)
-      end      
+    before(:each) do
+      @messenger = mock("messenger").as_null_object
+      @game = Game.new(@messenger)
+    end      
 
+    context "starting up" do
       it "should send a welcome message" do
         @messenger.should_receive(:puts).with("Welcome to Codebreaker!")
-        @game.start(%w[r c g y])
+        @game.start(%w[r g y c])
       end
 
       it "should prompt for the first guess" do
         @messenger.should_receive(:puts).with("Enter guess:")
-        @game.start(%w[r c g y])
-      end
-    end
-    
-    context "with all 4 colors correct in the correct places" do
-      it "should mark the guess with bbbb" do
-        messenger = mock("messenger").as_null_object
-        game = Game.new(messenger)
-        game.start(%w[r g y c])
-
-        messenger.should_receive(:puts).with("bbbb")
-
-        game.guess(%w[r g y c])
+        @game.start(%w[r g y c])
       end
     end
 
-    context "with all 4 colors correct and 2 in the correct places" do
-      it "should mark the guess with bbww" do
-        messenger = mock("messenger").as_null_object
-        game = Game.new(messenger)
-        game.start(%w[r g y c])
-
-        messenger.should_receive(:puts).with("bbww")
-
-        game.guess(%w[r g c y])
+    context "marking a guess" do
+      context "with all 4 colors correct in the correct places" do
+        it "should mark the guess with bbbb" do
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bbbb")
+          @game.guess(%w[r g y c])
+        end
       end
-    end
-
-    context "with all 4 colors correct and 1 in the correct place" do
-      it "should mark the guess with bwww" do
-        messenger = mock("messenger").as_null_object
-        game = Game.new(messenger)
-        game.start(%w[r g y c])
-
-        messenger.should_receive(:puts).with("bwww")
-
-        game.guess(%w[y r g c])
+      context "with all 4 colors correct and 2 in the correct places" do
+        it "should mark the guess with bbww" do
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bbww")
+          @game.guess(%w[r g c y])
+        end
+      end
+      context "with all 4 colors correct and 1 in the correct place" do
+        it "should mark the guess with bwww" do
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bwww")
+          @game.guess(%w[y r g c])
+        end
       end
     end
   end
